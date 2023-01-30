705973810c4ba3b3f24db95f476b4246aa3c923a
Shreyas Repale <98728490+ShreyasRepale12@users.noreply.github.com>
2023-01-30 15:43:23 +0530

    Add files via upload

---
+++ Practical/UnaryExample.java
@@ -0,0 +1,36 @@
+class UnaryExample{
+
+public void calcIncrement(){
+int x = 10;
+		System.out.println(x++); 
+		System.out.println(x);
+		System.out.println(x++);
+		System.out.println(x);
+		System.out.println(++x);
+		System.out.println(x);
+		System.out.println(x++);
+		System.out.println(x);
+}
+
+public void calcDecrement(){
+int x = 10;
+		System.out.println("---------");
+		System.out.println(x--);
+		System.out.println(x);
+		System.out.println(x--);
+		System.out.println(x);
+		System.out.println(--x);
+		System.out.println(x);
+		System.out.println(--x);
+		System.out.println(x);
+		System.out.println(x--);
+		System.out.println(x);
+}
+
+public static void main(String args[]){
+UnaryExample id = new UnaryExample();
+id.calcIncrement();
+id.calcDecrement();
+}
+}
+
